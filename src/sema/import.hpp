7c0841a975e6f94808790bb16e89e362d9993f63
bzjsmdl <bzjsmdl88@qq.com>
2026-09-12 19:58:44 +0800

    Add files via git~

--- src/sema/import.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ICE_SEMA_IMPORT_H
-#define ICE_SEMA_IMPORT_H
-#include "../lang.h"
-namespace sema {
-    class ImportPackage {
-        
-    };
-}
-#endif
--- src/sema/symbol.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef ICE_SEMA_SYMBOL_H
-#define ICE_SEMA_SYMBOL_H
-#include "../lang.h"
-namespace sema {
-	
-}
-#endif
