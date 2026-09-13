a398ebb69ede1099683e18f76f85e41bc8822006
bzjsmdl <bzjsmdl88@qq.com>
2026-09-13 21:49:36 +0800

    I'm alive~

--- src/include/rt.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "lang.h"
-
-extern std::vector<Operator> optable;
--- src/include/sema.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../include/lang.h"
-extern std::map<std::string, std::string> ImportFile;
-
-#include "../sema/eval.hpp"
-#include "../sema/import.hpp"
-#include "../sema/symbol.hpp"
-#include "../sema/main.hpp"
